a052e59083908bae6b6aeefc2991d4d17b2c6c78
us <rahmetsaritekin@gmail.com>
2026-06-28 14:12:25 +0000

    chore: update mocker to v0.5.4

--- Formula/mocker.rb
+++ Formula/mocker.rb
@@ -1,10 +1,10 @@
 class Mocker < Formula
   desc "Docker-compatible container CLI built on Apple's Containerization framework"
   homepage "https://github.com/us/mocker"
-  url "https://github.com/us/mocker/releases/download/v0.5.3/mocker-v0.5.3-arm64-apple-macosx.tar.gz"
-  sha256 "27c8f8d2d3c295dc280aacabbccb5dcad357f5598f5b4df2d50f28db9ebfda79"
+  url "https://github.com/us/mocker/releases/download/v0.5.4/mocker-v0.5.4-arm64-apple-macosx.tar.gz"
+  sha256 "edfa504db6960c05227d8a11182c0bb15c299d0616564e29fc8d24b51e56af28"
   license "MIT"
-  version "0.5.3"
+  version "0.5.4"
 
   def install
     bin.install "mocker"
@@ -20,6 +20,6 @@ class Mocker < Formula
   end
 
   test do
-    assert_match "0.5.3", shell_output("#{bin}/mocker --version")
+    assert_match "0.5.4", shell_output("#{bin}/mocker --version")
   end
 end
